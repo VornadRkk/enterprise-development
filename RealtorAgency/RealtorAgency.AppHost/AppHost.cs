b2cd45568ceb646770be396633b5107741617a12
VornadRkk <epra11111@mail.ru>
2025-12-11 18:30:49 +0400

    fixed

--- RealtorAgency/RealtorAgency.AppHost/AppHost.cs
+++ RealtorAgency/RealtorAgency.AppHost/AppHost.cs
@@ -17,6 +17,6 @@ builder.AddProject<Projects.RealtorAgency_Api>("RealtorAgencyApi")
 builder.AddProject<Projects.RealtorAgency_Producer>("RealtorAgencyProducer")
     .WithReference(rabbitMq)                      
     .WaitFor(rabbitMq)
-    .WithEnvironment("RABBITMQPUBLISHDELAYMS", "100");
+    .WithEnvironment("RabbitMqPublishDelayMs", "100");
 
 builder.Build().Run();
